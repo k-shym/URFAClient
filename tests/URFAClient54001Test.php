978046e0598d0fd3f877a4e95e9bf44e2566843c
Konstantin Shum <k.shym@ya.ru>
2022-01-01 18:22:54 +0300

    Add tests for UTM 5.4

---
+++ tests/URFAClient54001Test.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests;
+
+/**
+ * @package URFAClient
+ * @author  Konstantin Shum <k.shym@ya.ru>
+ * @license https://github.com/k-shym/URFAClient/blob/main/LICENSE.md GPLv3
+ */
+class URFAClient54001Test extends URFAClient54Test
+{
+    protected $config = [
+        'login'    => 'init',
+        'password' => 'init',
+        'address'  => 'utm',
+        'api'      => __DIR__ . '/../xml/api_54-001.xml',
+    ];
+}
---
+++ tests/URFAClient54Test.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests;
+
+/**
+ * Тесты для версии сервера 5.4
+ *
+ * @package URFAClient
+ * @author  Konstantin Shum <k.shym@ya.ru>
+ * @license https://github.com/k-shym/URFAClient/blob/main/LICENSE.md GPLv3
+ */
+abstract class URFAClient54Test extends URFAClient53006Test
+{
+}
