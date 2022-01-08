3f24c985d8159c635c9d6b1682706d06cc3d0416
Konstantin Shum <k.shym@ya.ru>
2022-01-08 13:06:45 +0300

    Tests for UTM 5.5

---
+++ tests/URFAClient55015Test.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests;
+
+/**
+ * @package URFAClient
+ * @author  Konstantin Shum <k.shym@ya.ru>
+ * @license https://github.com/k-shym/URFAClient/blob/main/LICENSE.md GPLv3
+ */
+class URFAClient55015Test extends URFAClient55Test
+{
+    protected $config = [
+        'login'    => 'init',
+        'password' => 'init',
+        'address'  => 'utm',
+        'api'      => __DIR__ . '/../xml/api_55-015.xml',
+    ];
+}
---
+++ tests/URFAClient55Test.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests;
+
+/**
+ * Тесты для версии UTM 5.5
+ *
+ * @package URFAClient
+ * @author  Konstantin Shum <k.shym@ya.ru>
+ * @license https://github.com/k-shym/URFAClient/blob/main/LICENSE.md GPLv3
+ */
+abstract class URFAClient55Test extends URFAClient54004Test
+{
+}
